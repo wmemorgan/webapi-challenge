0410b5da017989b7282654143f758c5fc50dc768
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-20 23:56:52 +0800

    Add Router component

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from './components/DesignComponents/GlobalStyle'
@@ -12,8 +13,10 @@ const root = document.getElementById('root')
 ReactDOM.render(
   <ThemeProvider theme={theme}>
     <Provider store={store}>
-      <GlobalStyle />
-      <App />
+      <Router>
+        <GlobalStyle />
+        <App />
+      </Router>
     </Provider>
   </ThemeProvider>,
   root

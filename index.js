44d98e5c8797d7011a87ad537b765afbf9c9c2cb
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-21 18:49:11 +0800

    Add conditional port number assignment

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const server = require(`./server`)
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT || 5000
 
 server.listen(PORT, () => {
   console.log(`Projects & Actions API server running on port ${PORT}`)

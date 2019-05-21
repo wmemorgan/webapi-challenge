2221a11dab1fe3ed1cb7095ae6dad0e08bd27883
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-05-21 18:11:07 +0800

    Add style and formatting to project actions components

---
+++ client/src/components/ActionComponents/ActionStyles.js
@@ -0,0 +1,155 @@
+import styled from 'styled-components'
+import { color, colorScheme, fontSizing, flex, breakpoints } from '../DesignComponents/theme'
+
+export const ActionBoardContainer = styled.div`
+  width: 100%;
+  max-width: 800px;
+  ${flex('column', 'center')};
+  margin-top: 20px;
+  padding: 10px;
+  border: 1px solid ${color.tx03};
+  border-radius: 5px;
+
+  h2 {
+      margin: 20px 0;
+      text-align: center;
+      color: ${color.secondaryBgShading};
+      font-size: ${fontSizing.s};
+  }
+`
+
+
+export const ActionFormContainer = styled.div`
+  width: 100%;
+   ${flex('column')}
+  
+  form {
+    width: 80%;
+    input {
+
+    }
+  }
+
+`
+
+export const FormContainer = styled.form`
+  width: 90%;
+  max-width: 600px;
+  font-size: ${fontSizing.xs};
+`
+
+export const FormGroup = styled.div`
+  width: 100%;
+  ${flex('row', 'center', 'space-between')}
+  
+  input {
+    width: 42%;
+    height: 40px
+    padding: 5px;
+    border: 1px solid ${colorScheme.defaultBorderColor};
+    font-size: ${fontSizing.xs};
+    background: ${color.txt01};
+    color: ${color.txt04};
+  }
+
+  button {
+    width: 10%;
+    height: 40px;
+    border: 1px solid ${colorScheme.defaultBorderColor};
+    border-radius: 5px;
+    background: ${color.txt01};
+    color: ${color.txt04};
+    font-size: ${fontSizing.m};
+  }
+
+  button:hover {
+    border: 1px solid ${color.accent0};
+  }
+`
+
+export const ActionListContainer = styled.div`
+    width: 90%;
+    max-width: 600px;
+    ${flex('column', 'flex-start')};
+
+    ul {
+      width: 100%;
+      padding: 0;
+      list-style-type: none;
+    }
+`
+
+export const ActionContainer = styled.div`
+    width: 100%;
+    ${flex('row', 'baseline', 'space-between')};
+    margin: 5px 0;
+    padding-left: 10px;
+    border: 1px solid ${colorScheme.defaultBorderColor};
+    border-radius: 5px;
+      background: ${color.txt01};
+
+    div {
+      flex: 0 0 43%;
+      padding: 5px 0;
+      font-size: ${fontSizing.xs};
+    }
+
+    div:last-child {
+      justify-self: 'flex-end';
+      flex: 0 0 14%;
+    }
+
+    i {
+      justify-self: 'flex-end';
+      font-size: ${fontSizing.xs};
+    }
+`
+
+export const ActionList = styled.div`
+    ${flex('row')};
+    width: 100%;
+    
+    @media ${breakpoints[0]} {
+      ${flex('column', 'center')};
+    }
+
+`
+
+export const ActionItemWrapper = styled.div`
+  width: 100%;
+  ${flex('column')};
+  flex: 0 0 80%;
+
+`
+
+export const ItemGroup = styled.div`
+  width: 100%;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+`
+
+export const Action = styled.div`
+    
+`
+
+export const DeleteContainer = styled.div`
+  flex: 0 0 10%;
+  ${flex('column', 'flex-end')};
+
+  .fa-ellipsis-v, .fa-edit {
+    margin: 5px;
+  }
+
+  .fa-ellipsis-v, .fa-trash, .fa-edit {
+    cursor: pointer;
+  }  
+`
+
+export const CheckBoxGroup = styled.div`
+  align-self: flex-start;
+  width: 85px;
+  ${flex('row', 'center', 'space-between')}
+  margin: 10px 0;
+  font-size: ${fontSizing.s};
+`
